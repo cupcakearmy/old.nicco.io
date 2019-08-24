18409912fb0a8f94b2c8ca3880db252ce25886c3
cupcakearmy <nicco.borgioli@gmail.com>
2019-08-24 10:00:18 +0200

    https & emphasis on privacy

--- src/Screens/Privacy.tsx
+++ src/Screens/Privacy.tsx
@@ -5,7 +5,7 @@ const Privacy: React.FC = () => {
 		<div className={'content'}>
 			<h2>Privacy Policy</h2>
 			<p>Your privacy is important to us. It is Nicco IO's policy to respect your privacy regarding any
-				information we may collect from you across our website, <a href="http://nicco.io">http://nicco.io</a>,
+				information we may collect from you across our website, <a href="https://nicco.io">https://nicco.io</a>,
 				and other sites we own and operate.</p>
 			<p>We only ask for personal information when we truly need it to provide a service to you. We collect it by
 				fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and
@@ -13,7 +13,7 @@ const Privacy: React.FC = () => {
 			<p>We only retain collected information for as long as necessary to provide you with your requested service.
 				What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as
 				well as unauthorised access, disclosure, copying, use or modification.</p>
-			<p>We don’t share any personally identifying information publicly or with third-parties, except when
+			<p><b>We don’t share any personally identifying information publicly or with third-parties</b>, except when
 				required to by law.</p>
 			<p>Our website may link to external sites that are not operated by us. Please be aware that we have no
 				control over the content and practices of these sites, and cannot accept responsibility or liability for
