1edb23826ffe80ad378f61eb9fbb94644a8d20b6
cupcakearmy <nicco.borgioli@gmail.com>
2019-08-30 20:21:32 +0200

    robots.txt

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: [
@@ -27,6 +28,9 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'bundle.css',
         }),
+        new CopyPlugin([
+            { from: 'Static/robots.txt', to: '.' },
+        ]),
     ],
     stats: {
         assets: true,
