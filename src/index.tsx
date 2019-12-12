fafaff3990ddea5d14558dd9b644275f8d54ca33
cupcakearmy <nicco.borgioli@gmail.com>
2019-12-12 15:26:28 +0100

    matomo

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.styl'
 
-//@ts-ignore
-const _paq = window._paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function () {
-    var u = "//stats.nicco.io/";
-    _paq.push(['setTrackerUrl', u + 'p_unicorns']);
-    _paq.push(['setSiteId', '1']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'j_unicorns'; s.parentNode.insertBefore(g, s);
-})();
 
 ReactDOM.render(<App />, document.getElementById('root'))
