7b5f7cfe866e5b301febd10d58f59ce3164ac93a
cupcakearmy <nicco.borgioli@gmail.com>
2020-01-05 17:34:49 +0100

    remove unecessary

--- components/Suspend.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Suspense } from 'react'
-
-
-const Suspend: (C: React.ComponentType) => React.ComponentType = (C) => () => {
-	return <Suspense fallback={<span>...</span>} children={<C/>}/>
-}
-
-export default Suspend
