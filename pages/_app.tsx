cf662aaed78910802be014bda9c149e0ad8de7d4
cupcakearmy <nicco.borgioli@gmail.com>
2020-01-08 16:06:19 +0100

    wrong page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default class extends App {
 		return (
 			<React.Fragment>
 				<Head>
-					<title>Alessandra Marten</title>
+					<title>nicco.io</title>
 					<meta name="viewport" content="width=device-width, initial-scale=1" />
 					<meta charSet="utf-8" />
 				</Head>
